01567b2ca067064fc0ad923c51a94849e2b99435
EBG1993 <167779198+EBG1993@users.noreply.github.com>
2024-04-22 19:56:24 -0600

    Update EBG1993.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/EBG1993.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/EBG1993.py
@@ -1,1 +1,18 @@
+# https://www.python.org
+
+""" esto es un comentario
+en comillas dobles
+"""
+
+my_variable = "mi varibale"
+my_variable = "nuevo valor"
+
+boleanos = True
+enteros = 1
+flotante  = 10.5
+string = " este es un string"
+
+print("Hola python")
+
+
 
